!1905 bug fix: 修复RC4微调attention mask处理问题 Merge pull request !1905 from glhyy/1.0.RC4

modellink/tasks/post_train/dpo/dpo_trainer.py
@@ -47,14 +47,12 @@ class DPOTrainer(BaseTrainer):
         args = get_args()
 
         if (not mpu.is_pipeline_first_stage()) and (not mpu.is_pipeline_last_stage()):
+            tokens, attention_mask = get_finetune_data_on_this_tp_rank(data_iterator)
             if args.variable_seq_lengths and args.pipeline_model_parallel_size > 2:
-                tokens, attention_mask = get_finetune_data_on_this_tp_rank(data_iterator)
-                labels, position_ids = None, None
+                return tokens, None, attention_mask, None
             else:
-                tokens, labels, attention_mask, position_ids = None, None, None, None
-
-            all_results = (tokens, labels, attention_mask, position_ids)
-            return all_results
+                return None, None, attention_mask, None
+                
         # Items and their type.
         keys = ['input_ids', 'attention_mask', 'labels']
         data_type = torch.int64