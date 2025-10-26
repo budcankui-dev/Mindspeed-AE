845b43a83ce9806f58ea56f3aa2b8fc151c03416
演嘉 <budcankui@qq.com>
2025-10-26 15:13:01 +0800

    A2Emlp创建

---
+++ mindspeed_llm/hetermoe/transformer/mlp.py
@@ -0,0 +1,65 @@
+
+from dataclasses import dataclass
+from typing import Optional, Tuple, Union
+
+import torch
+import torch.nn.functional as F
+
+from megatron.core import parallel_state
+from megatron.core.dist_checkpointing import ShardedTensor
+from megatron.core.dist_checkpointing.mapping import (
+    ReplicaId,
+    ShardedStateDict,
+    ShardedTensorFactory,
+)
+from megatron.core.fusions.fused_bias_geglu import bias_geglu_impl
+from megatron.core.fusions.fused_bias_gelu import bias_gelu_impl
+from megatron.core.fusions.fused_bias_swiglu import bias_swiglu_impl
+from megatron.core.transformer.module import MegatronModule
+from megatron.core.transformer.spec_utils import ModuleSpec, build_module
+from megatron.core.transformer.transformer_config import TransformerConfig
+from megatron.core.transformer.utils import make_sharded_tensors_for_checkpoint
+
+
+
+class HeterA2EMLP(MegatronModule):
+    """
+    MLP will take the input with h hidden state, project it to 4*h
+    hidden dimension, perform nonlinear transformation, and project the
+    state back into h hidden dimension.
+
+
+    Returns an output and a bias to be added to the output.
+    If config.add_bias_linear is False, the bias returned is None.
+
+    We use the following notation:
+     h: hidden size
+     p: number of tensor model parallel partitions
+     b: batch size
+     s: sequence length
+    """
+
+    def __init__(
+        self,
+        config: TransformerConfig,
+        input_size: int = None,
+    ):
+        super().__init__(config=config)
+
+        self.config: TransformerConfig = config
+
+        self.input_size = input_size if input_size != None else self.config.hidden_size
+
+    def set_layer_number(self, layer_number: int):
+        self.layer_number = layer_number
+
+    def forward(self, hidden_states):
+        
+
+        return hidden_states
+
+        
+
+    def backward(self, grad_output):
+        grad_input = grad_output
+        return grad_input
